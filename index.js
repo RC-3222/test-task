de50089955b80aa7e46e84cc113504f2f967c190
RC-3222 <99599598+RC-3222@users.noreply.github.com>
2023-06-28 15:38:05 +0300

    upload

---
+++ index.js
@@ -0,0 +1,102 @@
+const body = document.body;
+
+// --------------- Burger Stuff ---------------
+const openMenu = () => body.classList.add('menu-opened');
+const closeMenu = () => body.classList.remove('menu-opened');
+
+document.querySelector('.backdrop').addEventListener('click', closeMenu);
+
+document.querySelector('.burger-menu').addEventListener('click', function (event) {
+    if (event.target.classList.contains('burger-menu__link')) {
+        closeMenu();
+    }
+});
+
+document.querySelector('.burger-btn').addEventListener('click', openMenu);
+
+document.querySelector('.burger-menu__close-btn').addEventListener('click', closeMenu);
+
+
+// --------------- Form Stuff ---------------
+const form = document.querySelector('.section-contact__form');
+
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+
+const nameError = document.querySelector('.name-error');
+const emailError = document.querySelector('.email-error');
+const messageError = document.querySelector('.message-error');
+
+const handleRequest = async (data) => {
+    try {
+        const response = await fetch('some-api-url', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        // ...
+    } catch (err) {
+        throw err
+    }
+}
+
+const validateInputs = (nameValue, emailValue, messageValue) => {
+    const isNameValid = nameValue !== '';
+    const isEmailValid = (
+        emailValue !== ''
+        && !!emailValue.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)
+    );
+    const isMessageValid = messageValue.trim() !== '';
+
+    return { isNameValid, isEmailValid, isMessageValid };
+}
+
+const validateForm = () => {
+    const nameValue = nameInput.value.trim();
+    const emailValue = nameInput.value.trim();
+    const messageValue = nameInput.value.trim();
+
+    const { isNameValid, isEmailValid, isMessageValid } = validateInputs(nameValue, emailValue, messageValue);
+
+    // показ или скрытие ошибок
+    if (isNameValid) nameError.classList.add('hidden');
+    else nameError.classList.remove('hidden');
+
+    if (isEmailValid) emailError.classList.add('hidden');
+    else emailError.classList.remove('hidden');
+
+    if (isMessageValid) messageError.classList.add('hidden');
+    else messageError.classList.remove('hidden');
+
+    return (isNameValid && isEmailValid && isMessageValid);
+};
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // для предотвращения перезагрузки страницы по умолчанию
+
+    const isFormValid = validateForm();
+
+    if (!isFormValid) {
+        alert('Некорректные данные, запрос не отправлен');
+        return;
+    }
+
+    // сброс введённых значений
+    nameInput.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
+
+    // имитация запроса
+    handleRequest({
+        name: nameValue,
+        email: emailValue,
+        message: messageValue
+    }).catch((err) => console.error(err));
+})
